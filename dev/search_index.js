b2117c995978208698ecb94ac1da1015b4f1f556
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-11 21:04:45 +0000

    build based on c9d278b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Slacker.jl-1","page":"Home","title":"Slacker.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [Slacker]","category":"page"},{"location":"#Slacker.sendSlackMessage","page":"Home","title":"Slacker.sendSlackMessage","text":"sendSlackMessage(text, cfg::String = \"default\")\n\nSends a message to the configured default slack server or the one specified in cfg.\n\n\n\n\n\n","category":"function"}]
+}
